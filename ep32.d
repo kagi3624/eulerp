199cd1d6bd7f4f792f38a4609fd914ed54ad6089
Roman <roman.bleim@uni-oldenburg.de>
2015-10-02 16:33:02 +0200

    32

--- ep32.d
+++ ep32.d
@@ -1,23 +1,38 @@
-import std.stdio, std.algorithm, std.range; 
-
+import std.stdio, std.algorithm, std.array;
 
 void main(){
 
-
-int[] a = [1,2,3,4,5,6,7,8,9];
-
-
-/*do{
+	int[] n = [1,2,3,4,5,6,7,8,9];
+	int[] v;
 	
-	int p = (10*a[0]+a[1])*(100*a[2]+10*a[3]+a[4]);
+	do{
+		
+		int i;
+		
+		while(i<n.length-2){
+			int j = i+2;
+			int a;
+			for(int k; k<=i;++k)
+				a = 10*a + n[k];
+			while(j<n.length-1){
+				int b,c;
+				for(auto l = i+1; l<=j;++l)			
+					b = 10*b + n[l];
+
+				for(auto m = j+1;m<n.length;++m)
+					c = 10*c + n[m];
+				if(a*b == c){
+					v ~= c;	
+				}
+				writeln(a," ", b, " ", c);
+				++j;
+			}
+			++i;
+		}
+	}
+	while(nextEvenPermutation(n));
 	
-    
- while (nextPermutation(a)); */
-
-
-	writeln(a[0..3]);
-	writeln(a[$-3..$]);
-	writeln(a.take(3));
-	writeln(a.drop(a.length-3));
+	int[] arr = v.sort.uniq.array;
+	writeln(arr.sum());
 
 }
